e4378f2e6d38a9872dd382b30855aa0365903b3d
Igor Stalmach <igorstalmach@outlook.com>
2025-05-14 02:20:34 +0200

    Fix CORS

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.endpoints.ekg_endpoints import ekg_router
-from app.endpoints.health_endpoints import health_router
+from .endpoints.ekg_endpoints import ekg_router
+from .endpoints.health_endpoints import health_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://igorstalmach.github.io/ecg-visualizer/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type"]
+)
 
 app.include_router(ekg_router)
 app.include_router(health_router)
